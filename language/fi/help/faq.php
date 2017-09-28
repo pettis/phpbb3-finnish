9a190fc4e5bbac12067ee8f23e77cdef11844eb6
pettis <juha.petajajarvi@gmail.com>
2017-09-29 02:53:30 +0300

    Small header fixes

--- language/fi/help/faq.php
+++ language/fi/help/faq.php
@@ -1,13 +1,10 @@
 <?php
 /**
  *
- * This file is part of the phpBB Forum Software package.
+ * This file is part of the Finnish language pack
  *
- * @copyright (c) phpBB Limited <https://www.phpbb.com>
+ * @copyright (c) phpBB Suomi <https://www.phpbbsuomi.com>
  * @license GNU General Public License, version 2 (GPL-2.0)
- *
- * For full copyright and license information, please see
- * the docs/CREDITS.txt file.
  *
  */
 
